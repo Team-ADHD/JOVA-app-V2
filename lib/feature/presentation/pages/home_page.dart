9005ec4f8e0f82194e141b2ea62e0b49faf37a3c
이주언 <loolloo080313@gmailcom>
2025-01-20 11:54:29 +0900

    feat: homepage appbar 및 bottomNavigationbar 퍼블리싱

---
+++ lib/feature/presentation/pages/home_page.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:jova_v2/feature/presentation/getx/jova_getx.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final double screenHeight = MediaQuery.of(context).size.height;
+    final double appBarHeight = screenHeight * 0.15;
+    final double bottomNavigationBarHeight = screenHeight * 0.12;
+
+    final Jova_Controller controller = Get.put(Jova_Controller());
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(appBarHeight),
+          child: Container(
+            height: appBarHeight,
+            child: CupertinoNavigationBar(
+              backgroundColor: Colors.white,
+              padding: EdgeInsetsDirectional.symmetric(
+                horizontal: 30,
+                vertical: 10,
+              ),
+              leading: Builder(
+                  builder: (context){
+
+                    double logosize = appBarHeight * 0.6;
+
+                    return Image.asset(
+                        "image/assets/jova_logo.png",
+                      height: logosize,
+                      width: logosize,
+                    );
+                  },
+              ),
+              trailing: Icon(Icons.sunny),
+              border: Border.all(
+                color: Colors.grey,
+                width: 1,
+              ),
+            ),
+          ),
+        ),
+        body: Column(
+          children: [],
+        ),
+        bottomNavigationBar: Obx((){
+          return Container(
+            height: bottomNavigationBarHeight,
+            decoration: BoxDecoration(
+              border: Border(top: BorderSide(
+              color: Colors.grey,
+              width: 1
+            ),
+            ),
+          ),
+          child: BottomNavigationBar(
+            onTap: controller.changeindex,
+            backgroundColor: Colors.white,
+            elevation: bottomNavigationBarHeight,
+            type: BottomNavigationBarType.fixed,
+            currentIndex: controller.selectindex.value,
+            unselectedItemColor: Colors.black,
+            selectedItemColor: Colors.yellow,
+            items: [
+              BottomNavigationBarItem(icon: Icon(Icons.home),label: ""),
+              BottomNavigationBarItem(icon: Icon(Icons.description),label: ""),
+              BottomNavigationBarItem(icon: Icon(Icons.add_box_rounded,),label: ""),
+              BottomNavigationBarItem(icon: Icon(Icons.notifications),label: ""),
+              BottomNavigationBarItem(icon: Icon(Icons.person),label: ""),
+            ],
+          ),
+        );
+      })
+      ),
+    );
+  }
+}
