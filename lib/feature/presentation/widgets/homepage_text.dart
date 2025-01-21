df9ca81023753e86fa8553992d610ea93136c475
이주언 <loolloo080313@gmailcom>
2025-01-21 13:28:44 +0900

    feat: 필터안에 들어갈 text 디자인 및 글자

---
+++ lib/feature/presentation/widgets/homepage_text.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/cupertino.dart';
+
+import '../color/color.dart';
+
+class HomepageText extends StatelessWidget {
+  const HomepageText({super.key, required this.major});
+
+  final String major;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: Text.rich(
+        TextSpan(
+          children: [
+            TextSpan(
+              text:"$major",
+              style: TextStyle(
+                  fontFamily: 'PretendardVariable',
+                  fontWeight: FontWeight.w800,
+                  decoration: TextDecoration.underline,
+                  decorationColor: AppColors.yellowcolor,
+                  decorationThickness: 4
+              ),
+            ),
+            TextSpan(
+              text:"를 찾는 ",
+              style: TextStyle(
+                  fontFamily: 'PretendardVariable',
+                  fontWeight: FontWeight.w800,
+              ),
+            ),
+            TextSpan(
+              text:"구인구직",
+              style: TextStyle(
+                  fontFamily: 'PretendardVariable',
+                  fontWeight: FontWeight.w800,
+                  decoration: TextDecoration.underline,
+                  decorationColor: AppColors.yellowcolor,
+                  decorationThickness: 4
+              ),
+            ),
+          ]
+        ),
+      ),
+    );
+  }
+}
